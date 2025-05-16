85cb8d5fab5576a5093afdbc00b324e0ff3bae14
Wilfredo <wilfredopintomata@gmail.com>
2025-05-16 19:19:47 +0200

    Navbar and Jumbotron components finished and working

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,27 +1,15 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import HeroComponent from "./HeroComponent";
+import NavbarComponent from "./NavbarComponent";
 
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<NavbarComponent />
+			<div className="mx-5 px-4 px-md-5 my-5">
+				<HeroComponent />
+			</div>
+		</>
 	);
 };
 
