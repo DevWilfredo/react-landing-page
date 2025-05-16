45a41b33bdb82d84c8da9b76040f9f48af472cf1
Wilfredo <wilfredopintomata@gmail.com>
2025-05-16 19:31:24 +0200

    Add props to components to make them dinamic and reusable

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,13 +1,25 @@
 import HeroComponent from "./HeroComponent";
 import NavbarComponent from "./NavbarComponent";
 
+
+const navbarItems = [
+	{ link: '#', text: 'Home' },
+	{ link: '#', text: 'About' },
+	{ link: '#', text: 'Services' },
+	{ link: '#', text: 'Contact' }
+]
+
 //create your first component
 const Home = () => {
 	return (
 		<>
-			<NavbarComponent />
+			<NavbarComponent items={navbarItems} brandText='Start Boostrap' />
 			<div className="mx-5 px-4 px-md-5 my-5">
-				<HeroComponent />
+				<HeroComponent
+					title='A Warm Welcome!'
+					textBody="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam commodi tenetur labore, placeat quae dolore, ea temporibus est incidunt repudiandae minus delectus laudantium repellat dolor dolores sit atque consequatur in?"
+					buttonText='Call to Action'
+				/>
 			</div>
 		</>
 	);
--- src/js/components/NavbarComponent.jsx
+++ src/js/components/NavbarComponent.jsx
@@ -1,8 +1,8 @@
-const NavbarComponent = () => {
+const NavbarComponent = ({ items, brandText }) => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid px-5 py-2">
-                <a className="navbar-brand" href="#">Start Boostrap</a>
+                <a className="navbar-brand" href="#">{brandText}</a>
 
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
@@ -12,18 +12,11 @@ const NavbarComponent = () => {
 
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">About</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Services</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Contact</a>
-                        </li>
+                        {items.map((item) => (
+                            <li className="nav-item">
+                                <a className="nav-link" aria-current="page" href={item.link}>{item.text}</a>
+                            </li>
+                        ))}
                     </ul>
                 </div>
             </div>
