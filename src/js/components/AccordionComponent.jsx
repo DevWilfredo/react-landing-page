1b117a6f929ecdcb0b103195184b6c284d4b6f22
jusspian <luisfjuspian@gmail.com>
2025-05-18 15:16:18 +0000

    fixed

--- src/js/components/AccordionComponent.jsx
+++ src/js/components/AccordionComponent.jsx
@@ -1,39 +1,30 @@
-const AccordionComponent = () => {
+const AccordionComponent = ({items, index}) => {
   return (
-    <div class="accordion accordion-flush" id="accordionA">
-      
-      <div class="accordion-item">
+    <div class="accordion accordion-flush" id={`accordion-${index}`}>
+      {items.map((item, idx) => (
+      <div class="accordion-item" key={idx}>
         <h2 class="accordion-header">
-          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseA">
-            This is the first Item
-          </button>
-        </h2>
-        <div id="collapseA" class="accordion-collapse collapse" data-bs-parent="#accordionA">
-          <div class="accordion-body">Just a demos.</div>
-        </div>
-      </div>
 
-      <div class="accordion-item">
-        <h2 class="accordion-header">
-          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseB">
-            This is the second Item
+          <button 
+          class="accordion-button collapsed" 
+          type="button" 
+          data-bs-toggle="collapse" 
+          data-bs-target={`#collapse-${index}-${idx}`}
+          >
+          {item.title}
           </button>
-        </h2>
-        <div id="collapseB" class="accordion-collapse collapse" data-bs-parent="#accordionA">
-          <div class="accordion-body">Just a demos.</div>
-        </div>
-      </div>
 
-      <div class="accordion-item">
-        <h2 class="accordion-header">
-          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseC">
-            This is the third Item
-          </button>
         </h2>
-        <div id="collapseC" class="accordion-collapse collapse" data-bs-parent="#accordionA">
-          <div class="accordion-body">Just a demos.</div>
+
+        <div 
+        id={`collapse-${index}-${idx}`} 
+        class="accordion-collapse collapse" 
+        data-bs-parent={`#accordion-${index}`}
+        >
+          <div class="accordion-body">{item.content}</div>
         </div>
       </div>
+    ))}
     </div>
   );
 };
