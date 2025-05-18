6a032b3b11ba441b71416530c3fab1e104945cef
jusspian <luisfjuspian@gmail.com>
2025-05-18 12:09:30 +0000

    just minimies

--- src/js/components/Accordion2Component.jsx
+++ src/js/components/Accordion2Component.jsx
@@ -16,7 +16,7 @@ const AccordionComponent2 = () => {
       <div class="accordion-item">
         <h2 class="accordion-header">
           <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseE">
-            This is the first Item but in the other Accordion
+            This is the Second Item but in the other Accordion
           </button>
         </h2>
         <div id="collapseE" class="accordion-collapse collapse" data-bs-parent="#accordionB">
@@ -27,7 +27,7 @@ const AccordionComponent2 = () => {
       <div class="accordion-item">
         <h2 class="accordion-header">
           <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseF">
-            This is the first Item but in the other Accordion
+            This is the Third Item but in the other Accordion
           </button>
         </h2>
         <div id="collapseF" class="accordion-collapse collapse" data-bs-parent="#accordionB">
