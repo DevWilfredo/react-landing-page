28ef21fe9452f428d2da38c6178ac4cebbe27d1e
HannahAlAssad <hannahalassad@gmail.com>
2025-05-18 21:10:03 +0000

    cards

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -2,8 +2,7 @@ import HeroComponent from "./HeroComponent";
 import NavbarComponent from "./NavbarComponent";
 import AccordionComponent from "./AccordionComponent";
 import FooterComponent from "./FooterComponent";
-
-
+import CardsComponent from "./CardsComponent";
 
 const navbarItems = [
 	{ link: '#', text: 'Home' },
@@ -12,18 +11,37 @@ const navbarItems = [
 	{ link: '#', text: 'Contact' }
 ]
 
+const cardSItems = [
+	{
+		title: "Card title",
+		text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+	},
+	{
+		title: "Card title",
+		text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+	},
+	{
+		title: "Card title",
+		text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation "
+	},
+	{
+		title: "Card title",
+		text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+	},
+]
+
 const accordionItems = [
-	{title: 'First Item', content:'Lorem ipsum dolor sit, amet consectetur adipisicing elit. '},
-	{title: 'Second Item', content:'Lorem ipsum dolor sit, amet consectetur adipisicing elit. '},
-	{title: 'Third Item', content:'Lorem ipsum dolor sit, amet consectetur adipisicing elit. '},
-	{title: 'Fourth Item', content:'Lorem ipsum dolor sit, amet consectetur adipisicing elit. '},
+	{ title: 'First Item', content: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. ' },
+	{ title: 'Second Item', content: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. ' },
+	{ title: 'Third Item', content: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. ' },
+	{ title: 'Fourth Item', content: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. ' },
 ]
 
 const accordionItems2 = [
-	{title: 'Another item', content:'Lorem ipsum dolor sit, amet consectetur adipisicing elit. '},
-	{title: 'and another..', content:'Lorem ipsum dolor sit, amet consectetur adipisicing elit. '},
-	{title: 'another one...', content:'Lorem ipsum dolor sit, amet consectetur adipisicing elit. '},
-	{title: 'yess, the last', content:'Lorem ipsum dolor sit, amet consectetur adipisicing elit. '},
+	{ title: 'Another item', content: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. ' },
+	{ title: 'and another..', content: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. ' },
+	{ title: 'another one...', content: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. ' },
+	{ title: 'yess, the last', content: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. ' },
 ]
 
 //create your first component
@@ -37,20 +55,28 @@ const Home = () => {
 					textBody="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam commodi tenetur labore, placeat quae dolore, ea temporibus est incidunt repudiandae minus delectus laudantium repellat dolor dolores sit atque consequatur in?"
 					buttonText='Call to Action'
 				/>
+
 				<div class="row mt-5">
 					<div class="col-md-6">
-				<AccordionComponent items = {accordionItems} index={1}/>
-				</div>
+						<AccordionComponent items={accordionItems} index={1} />
+					</div>
 
-				<div class="col-md-6">
-				<AccordionComponent items = {accordionItems2} index={2}/>
-				</div>
+					<div class="col-md-6">
+						<AccordionComponent items={accordionItems2} index={2} />
+					</div>
 
-				</div>				
-			</div>			
-			<FooterComponent/>
+				</div>
+			</div>
+			<FooterComponent />
+			<div className="row px-4 px-md-5 my-5">
+				{
+					cardSItems.map((card, index) => (
+						<CardsComponent key={index} card={card} />
+					))
+				}
+			</div>
 		</>
-		
+
 	);
 };
 
