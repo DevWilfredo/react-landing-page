8363de159583e7d3a6f77776ed115c619ffd731b
HannahAlAssad <hannahalassad@gmail.com>
2025-05-18 01:03:42 +0000

    agregar cards

---
+++ src/js/components/CardComponent.jsx
@@ -0,0 +1,16 @@
+export const CardComponent = ({ card }) => {
+  
+  return (
+    <div className="col-md-3 col-sm-6">
+
+      <div class="card">
+        <img src="https://laclosette.com/wp-content/uploads/2017/10/500x300.png" class="card-img-top" alt="..."/>
+          <div class="card-body">
+            <h5 class="card-title">{card.title}</h5>
+            <p class="card-text" style={{ minHeight: "200px" }}>{card.text}</p>
+            <a href="#" class="btn btn-primary">Find Out More!</a>
+          </div>
+      </div>
+    </div>
+  )
+}
--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -9,6 +9,25 @@ const navbarItems = [
 	{ link: '#', text: 'Contact' }
 ]
 
+const cardSItems = [
+	{
+		title: "Card title",
+		text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+	},
+	{
+		title: "Card title",
+		text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+	},
+	{
+		title: "Card title",
+		text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation "
+	},
+	{
+		title: "Card title",
+		text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+	},
+]
+
 //create your first component
 const Home = () => {
 	return (
@@ -20,6 +39,14 @@ const Home = () => {
 					textBody="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam commodi tenetur labore, placeat quae dolore, ea temporibus est incidunt repudiandae minus delectus laudantium repellat dolor dolores sit atque consequatur in?"
 					buttonText='Call to Action'
 				/>
+			</div>
+
+			<div className="row px-4 px-md-5 my-5">
+				{
+					cardSItems.map((card, index) => (
+						<CardComponent key={index} card={card} />
+					))
+				}
 			</div>
 		</>
 	);
